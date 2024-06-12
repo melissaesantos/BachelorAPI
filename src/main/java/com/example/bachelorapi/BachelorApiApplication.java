a1d866d91fc3cd6ff4e79e9eba14bb725ef9dbda
Melissa Santos <melissasantos2202@gmail.com>
2024-06-12 09:14:40 -0700

    Created bachelor API files

---
+++ src/main/java/com/example/bachelorapi/BachelorApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.bachelorapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BachelorApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BachelorApiApplication.class, args);
+	}
+
+}
