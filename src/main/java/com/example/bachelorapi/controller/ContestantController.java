422dc16613b17b469158d127c3d5eecdeeb4e935
Melissa Santos <melissasantos2202@gmail.com>
2024-06-13 10:32:32 -0700

    updating contestant @PutMapping

--- src/main/java/com/example/bachelorapi/controller/ContestantController.java
+++ src/main/java/com/example/bachelorapi/controller/ContestantController.java
@@ -3,6 +3,7 @@ package com.example.bachelorapi.controller;
 import com.example.bachelorapi.model.Contestant;
 import com.example.bachelorapi.repository.ContestantRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.jackson.JsonComponentModule;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,6 +15,8 @@ public class ContestantController {
     @Autowired
     //allows the controller to use the repository for CRUD operations on the 'Contestant' entity
     private ContestantRepository contestantRepository;
+    @Autowired
+    private JsonComponentModule jsonComponentModule;
 
     @GetMapping
     public List<Contestant> getAllContestants() {
@@ -31,6 +34,19 @@ public class ContestantController {
     @PostMapping("/{id}")
     public Contestant createContestant(@RequestBody Contestant contestant) {
         return contestantRepository.save(contestant);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Contestant> updateContestant(@PathVariable Long id, @RequestBody Contestant contestantDetails){
+        return contestantRepository.findById(id)
+                .map(contestant -> {
+                    contestant.setName(contestantDetails.getName());
+                    contestant.setAge(contestantDetails.getAge());
+                    contestant.setOccupation(contestantDetails.getOccupation());
+                    contestant.setSeason(contestantDetails.getSeason());
+                    Contestant updatedContestant = contestantRepository.save(contestant);
+                    return ResponseEntity.ok().body(updatedContestant);
+                }).orElse(ResponseEntity.notFound().build());
     }
 
 
