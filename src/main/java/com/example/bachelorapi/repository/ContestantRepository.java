cacad6c3e387bab469426a4d14b7517fc734fedb
Melissa Santos <melissasantos2202@gmail.com>
2024-06-13 09:59:42 -0700

    set up contestant repository

--- src/main/java/com/example/bachelorapi/repository/ContestantRepository.java
+++ src/main/java/com/example/bachelorapi/repository/ContestantRepository.java
@@ -1,4 +1,8 @@
 package com.example.bachelorapi.repository;
 
-public class ContestantRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.example.bachelorapi.model.Contestant;
+// this repository will manage 'contestant' enteties and the type of the primary key ('id') is Long
+public interface ContestantRepository extends JpaRepository<Contestant, Long> {
+    //add custom query methods
 }
