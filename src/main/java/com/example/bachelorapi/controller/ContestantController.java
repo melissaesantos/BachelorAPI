ae07f91f102219c75842c84b25bc8ea47cba2901
Melissa Santos <melissasantos2202@gmail.com>
2024-06-13 10:17:47 -0700

    ContestantController setup

--- src/main/java/com/example/bachelorapi/controller/ContestantController.java
+++ src/main/java/com/example/bachelorapi/controller/ContestantController.java
@@ -1,4 +1,18 @@
 package com.example.bachelorapi.controller;
 
+import com.example.bachelorapi.model.Contestant;
+import com.example.bachelorapi.repository.ContestantRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/contestants")
 public class ContestantController {
+    @Autowired
+    //allows the controller to use the repository for CRUD operations on the 'Contestant' entity
+    private ContestantRepository contestantRepository;
+
 }
