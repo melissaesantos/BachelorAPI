6110b81ea1f6cb28b8159ed0e59d9174347728cb
Melissa Santos <melissasantos2202@gmail.com>
2024-06-12 22:40:13 -0700

    declaring contestant args

--- src/main/java/com/example/bachelorapi/model/Contestant.java
+++ src/main/java/com/example/bachelorapi/model/Contestant.java
@@ -1,4 +1,21 @@
 package com.example.bachelorapi.model;
 
+import jakarta.persistence.Entity;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Contestant {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    //declaring what the contestant will consist of
+    private Long id;
+    private String name;
+    private int age;
+    private String Occupation;
+    private String season;
+
 }
